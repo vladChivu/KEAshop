5b0a01de7c0c551b015e5da03500bba27d299054
vladChivu <vlad0888@stud.kea.dk>
2022-03-26 11:06:47 +0100

    added fauna db conn

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.json());
 
+// AUTHENTICATION
 const { auth } = require("express-openid-connect");
 require('dotenv').config();
 const config = {
@@ -17,6 +19,11 @@ const config = {
 app.use(auth(config));
 const { requiresAuth } = require("express-openid-connect");
 
+// DATABASE (Fauna)
+const faunadb = require("faunadb"), q = faunadb.query;
+const client = new faunadb.Client({secret: process.env.FAUNADB_SECRET_KEY});
+
+// ROUTES
 // req.isAuthenticated is provided from the auth router
 app.get("/", (req, res) => {
     res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
@@ -26,8 +33,27 @@ app.get("/callback", (req, res) => {
     res.send({data: "You are in the shop"});
 });
 
-app.get('/profile', requiresAuth(), (req, res) => {
+app.get("/profile", requiresAuth(), (req, res) => {
     res.send(JSON.stringify(req.oidc.user));
+});
+
+app.get("/shop", async(req, res) => {
+    try {
+        // const createP = await client.query(
+        const products = await client.query (
+            // q.Create(q.Collection('products'), { data: { testField: 'testValue' } })
+            q.Map(
+                q.Paginate(q.Documents(q.Collection("products"))),
+                q.Lambda("X", q.Get(q.Var("X")))
+            )
+        )
+
+        res.status(200).json(products)
+        // console.log(createP);
+    } catch (error) {
+        // console.log(error);
+        res.status(500).json({error: error.description})
+    }
 });
 
 const PORT = process.env.PORT || 3000;
